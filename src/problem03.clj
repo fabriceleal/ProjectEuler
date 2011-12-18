bf770137804494371d85f98aaa757a2fc9c9cfc9
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-18 15:27:22 +0000

    problem 03 almost done

--- src/problem03.clj
+++ src/problem03.clj
@@ -1,15 +1,29 @@
 (ns problem03)
 
+; Infinite decreasing stream, stop at 1
+(defn lazy-stream [start]
+  (if 
+    (< start 2) 
+    '()
+    (lazy-seq (cons start (lazy-stream (- start 1))))))
+
+; Test if two nbrs are divisible
+(defn is-divisor-of [toTest nbr]
+  (= 0 (mod nbr toTest)))
+
+; Get lazy list of divisors of nbr
+(defn divisors [nbr]
+  (let [stream (lazy-stream (- nbr 1))]
+    (lazy-seq (filter (fn[n](is-divisor-of n nbr)) stream))))
+
 ; Check if number is prime
 (defn is-prime? [n]
-  true)
-
-(defn lazy-stream [start]
-  (lazy-seq (cons start (lazy-stream (- start 1)))))
+  (nil? (first (divisors n))))
+(def is-prime-main (memoize is-prime?))
 
 ; Find first greatest prime factor of number ...
 (defn get-first-prime-factor [n]
-  n)
+  (first (divisors n)))
 
 
-(get-first-prime-factor 600851475143)
+;(get-first-prime-factor 600851475143)
