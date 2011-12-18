d06f42a0bfa8eefcaf2c8cb900dde65504feb2d0
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-18 13:41:55 +0000

    problem 01 solved

--- src/problem01.clj
+++ src/problem01.clj
@@ -1,5 +1,17 @@
 (ns problem01)
 
+; Aux function for multiples 
+(defn aux-multiples [n i top soFar]
+  (let [value (* n i)]
+    (if (>= value top)
+      soFar
+      (aux-multiples n (+ 1 i) top (concat soFar (list value)))
+    )))
+
 ; Generate multiples until 1000
-(def multiples(n)
-  )
+(defn multiples [n]
+  (aux-multiples n 1 1000 '()))
+
+; Sum the items in the list of multiples of 3 and 5, remove duplicates
+(defn solve []
+  (apply + (distinct (concat (multiples 3) (multiples 5)))))
