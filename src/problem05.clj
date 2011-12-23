470841472eb7e22c96c9552e47174ff9916e3218
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-23 23:27:06 +0000

    need to implement gcd and mcm

--- src/problem05.clj
+++ src/problem05.clj
@@ -29,15 +29,24 @@
         ))))
 
 ; Brute force by multiplying :)
-; If 20 is divisor, so it is 2 4 5 10
+; Test periodicity of the divisory properties of the other tests (11 for instance). 
+; Use increments of it to generate new numbers.
+
+; If 20 is divisor, so it is 2 4 5 10. Although the numbers generated are all multiples of 20, test just to skip 2, 4, 5 and 10
 ; 19 is prime
 ; If 18 is divisor, so it is 2 3 6 9
 ; 17 is prime
 ; If 16 is divisor, so it is 2 4 8
-; if 15 is divisor, so it is 3 5
+; if 15 is divisor, so it is 3 5 
 ; If 14 is divisor so it is 2 7
 ; 13 is prime
 ; If 12 is divisor so it is 2 3 4 6
 ; 11 is prime
 
-;(get-first-with-divisors 20 20 (reverse (range 10 21)))
+(get-first-with-divisors 20 20 (list 20 19 18 17 15 16 14 13 12 11))
+
+(defn least-commmon-multiplier [items]
+  )
+
+(defn greatest-common-divisor [items]
+  )
