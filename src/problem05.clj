043dbd727dfa2d3527b6bac9c6b6af179a3c3067
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-24 12:46:36 +0000

    problem 05 solved with prime factorization algorithm

--- src/problem05.clj
+++ src/problem05.clj
@@ -1,6 +1,7 @@
 (ns problem05)
 
 (refer 'problem03)
+(refer 'problem16)
 
 (defn list-contains-items [listone listitems]
   "All in listitems should be in listone"
@@ -20,13 +21,13 @@
   ([start] (lazy-seq (cons 1 (infinite-stream (inc start))))))
 
 ; Brute force by dividing, incrementing. Forget this.
-(defn get-first-with-divisors [start incr divs]
-  (loop [s start]
-    (let [real (divisors s)]
-      (if (list-contains-items real divs)
-        s
-        (recur (+ incr s))
-        ))))
+;(defn get-first-with-divisors [start incr divs]
+;  (loop [s start]
+;    (let [real (divisors s)]
+;      (if (list-contains-items real divs)
+;        s
+;        (recur (+ incr s))
+;        ))))
 
 ; Brute force by multiplying :)
 ; Test periodicity of the divisory properties of the other tests (11 for instance). 
@@ -53,3 +54,63 @@
         (recur (rest to-parse) (concat acc (list (get-factors head))))))))
 
 (serial-factorization '(11 12 13 14 15 16 17 18 19 20))
+
+(defn remove-first [list-items to-chop]
+  (let [i 0]
+    (filter 
+      #(and (= i 0) (= % to-chop)) 
+      list-items)))
+
+; Unfinished, might be usefull
+;(defn put-in-evidence [list-items]
+;  (loop [to-parse list-items evidence '()]
+;    ; Use 1st list as base. It should be the shortest.
+;    ; As soon as it is empty, there are no more common items
+;    (let [find-me (first (first to-parse))]
+;      (if (nil? find-me)
+;        (list (list evidence) (list to-parse)) 
+;        ; Try to find if find-me occur in all lists. 
+;        ; If it does, remove one instance of all lists and resume processing ...
+;        
+;        ))))
+
+; Make hash, prime => count, with the max ocorrences of a number in a list
+(defn decompose [list-list-items]
+  (loop [ret-hash {} to-parse list-list-items]
+    (let [head (first to-parse)]
+      (if (nil? head)
+        ret-hash
+        (recur (decompose-list head ret-hash) (rest to-parse))))))
+
+(defn decompose-list 
+  ([list-items] 
+    (decompose-list list-items {}))
+  ([list-items default-acc]
+    (loop [to-parse list-items acc default-acc]
+      (let [head (first to-parse)]
+        (if (nil? head)
+          acc
+          (recur 
+            (filter #(not= % head) to-parse) 
+            (assoc 
+              acc 
+              head 
+              (max 
+                (count (filter #(=% head) to-parse)) 
+                (get acc head 0)))))
+        ))))
+
+; http://en.wikipedia.org/wiki/Least_common_multiple
+(reduce 
+  (fn[acc v] (* acc (power (first v) (second v)))) ; 
+  1
+  (map 
+    (fn[n] n) 
+    (decompose (serial-factorization (range 2 21)))))
+
+; This is not an implementation of the lcm algorithm.
+; Reduce the result by multipliying all the items on the list
+
+;(reduce * (mapcat (fn[n] n) (serial-factorization (list 10 9 8 7 6))))
+;(reduce * (mapcat (fn[n] n) (serial-factorization (range 2 20))))
+;(reduce * (mapcat (fn[n] n) (serial-factorization '(11 12 13 14 15 16 17 18 19 20))))
