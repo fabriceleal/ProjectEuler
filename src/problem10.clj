e541e0a8a13f080735e757d22ba3c9861acffa76
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-26 21:43:44 +0000

    commented dummy prime generation, try clever prime generation

--- src/problem10.clj
+++ src/problem10.clj
@@ -3,47 +3,41 @@
 ; Generate list of prime number up to 2M
 (refer 'problem03)
 
-;(defn primes-iteration [ls transformed]
-;  ; Get first element at ls (it is prime)
-;  ; put in transformed
-;  ; Remove all multiples of elem in ls (including elem)
-;  ; Next iteration
-;  ; Return transformed
-;  (let [head (first ls)]
-;    (if (nil? head)
-;      transformed
-;      (primes-iteration
-;        (filter #(not (is-divisor-of head %)) ls)
-;        (concat transformed (list head))
-;        ))))
-
-(defn primes-iteration [ls]
-  ; Get first element at ls (it is prime)
-  ; put in transformed
-  ; Remove all multiples of elem in ls (including elem)
-  ; Next iteration
-  ; Return transformed
-  (loop [transformed '() pr-ls ls]
-    (let [head (first pr-ls)]
-      (if (not (nil? head)) 
-        (recur 
-          (concat (list head) transformed) 
-          (filter #(not (is-divisor-of head %)) pr-ls))
-        transformed))))
+(defn sieve-eristh [under]
+  (let [my-primes (range 2 under)]
+    (loop [left-to-parse my-primes 
+           parsed-list '()]
+      (let [car (first left-to-parse)]
+        (if (nil? car)
+          parsed-list
+          (let 
+            [filtered (filter #(not= (mod % car) 0) left-to-parse)] 
+            (recur filtered (concat parsed-list (list car)))))))))
 
 
-(defn primes [top]
-  ; Generate full list
-  (let [full (lazy-stream-inc 2 top)]
-    ; Use 
-    (primes-iteration full)))
+(defn sieve-eristh-2 [under]
+  (let [my-primes {}]
+    (loop [i 2
+           acc my-primes]
+      ; Get next nbr. If is not marked, is prime, set to true ... set multiples to false
+      (let [is-prime (get my-primes i true)
+            multiples (take-while #(<= % under) (apply (fn this[x] (lazy-seq (cons x (this (+ x i))))) (list i)))] ; lazy list
+
+          ; Mark as prime, mark multiples as not-primes, go to next
+          ;else
+          ; Mark as not-prime, go to next
+          
+        
+        )
+      
+      )))
 
 ;(reduce + 
 ;        (filter is-prime? 
 ;                (lazy-stream-inc 2 2000000)))
 
-(ns tokenize
-  (:import (java.io BufferedReader FileReader)))
+;(ns tokenize
+;  (:import (java.io BufferedReader FileReader)))
 
 ;(defn process-file [file-name line-func line-acc]
 ;  (with-open [rdr (BufferedReader. (FileReader. file-name))]
