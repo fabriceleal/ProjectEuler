f0d4a47f0c87fff0a443638246f73288731455a9
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-26 22:09:32 +0000

    got to find fast factorization algorithm

--- src/problem12.clj
+++ src/problem12.clj
@@ -27,11 +27,11 @@
 
 ; Lazy list of 
 (defn find-first-over-x [v]
-  (loop [ttt (triang 5000000000)]
+  (loop [ttt (triang 100000000000)]
     (let [head (first ttt)]
       (if (< v (count-divisors head))
         head
         (recur (rest ttt))))))
 
-(find-first-over-x 498) ; divisors don't include neither 1 nor the nbr itself 
+;(find-first-over-x 498) ; divisors don't include neither 1 nor the nbr itself 
 
